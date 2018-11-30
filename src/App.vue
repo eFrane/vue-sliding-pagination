41c3caf00d38d509a2e7a1abe2598617b5455e89
eFrane <efrane@meanderingsoul.com>
2018-11-30 11:13:22 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>vue-sliding-pagination</h1>
+    <p>
+      A simple pagination component which
+      provides sliding window pagination events
+      and can be heavily customized.
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
