3a89dfccecf594b7f1405a9712c767daf3cc1eca
eFrane <efrane@meanderingsoul.com>
2018-12-11 13:42:25 +0100

    Add browser build plugin main

---
+++ src/plugin.js
@@ -0,0 +1,7 @@
+import SlidingPagination from './SlidingPagination.vue'
+
+module.exports = {
+  install: function (Vue, options) {
+    Vue.component('SlidingPagination', SlidingPagination)
+  }
+}
