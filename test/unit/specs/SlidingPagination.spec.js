a362e17f2ab44425e06cf8f3309317f3aff00215
François Hervieux <francois.hervieux@gmail.com>
2021-02-26 23:21:10 +0100

    Add a test case for custom component page-click arguments

--- test/unit/specs/SlidingPagination.spec.js
+++ test/unit/specs/SlidingPagination.spec.js
@@ -248,24 +248,36 @@ describe('SlidingPagination.vue', () => {
 })
 
 describe('SlidingPagination.vue-custom', () => {
-  it('supports replacing the default page component', () => {
-    const localVue = createLocalVue()
+  const localVue = createLocalVue()
 
-    localVue.component('TestPageComponent', {
-      name: 'TestPageComponent',
+  localVue.component('TestPageComponent', {
+    name: 'TestPageComponent',
 
-      props: {
-        page: {
-          type: Number,
-          required: true
-        }
-      },
-
-      render (h) {
-        return h('a', { class: 'test-page-component' }, 'Test Page ' + this.page)
+    props: {
+      page: {
+        type: Number,
+        required: true
       }
-    })
+    },
 
+    render (h) {
+      const self = this
+      return h(
+        'a',
+        {
+          class: `test-page-component-${this.page}`,
+          on: {
+            click: function () {
+              self.$emit('page-click', 42)
+            }
+          }
+        },
+        'Test Page ' + this.page
+      )
+    }
+  })
+
+  it('supports replacing the default page component', () => {
     const wrapper = mount(SlidingPagination, {
       localVue,
       propsData: {
@@ -275,6 +287,23 @@ describe('SlidingPagination.vue-custom', () => {
       }
     })
 
-    expect(wrapper.find('.test-page-component').text()).toBe('Test Page 1')
+    expect(wrapper.find('.test-page-component-1').text()).toBe('Test Page 1')
+  })
+
+  it('relies on page component arguments for changing page', () => {
+    const wrapper = mount(SlidingPagination, {
+      localVue,
+      propsData: {
+        current: 1,
+        total: 3,
+        pageComponent: 'TestPageComponent'
+      }
+    })
+
+    wrapper.find('.test-page-component-1').trigger('click')
+
+    expect(wrapper.emitted()['page-change']).toBeTruthy()
+    expect(wrapper.emitted()['page-change'].length).toBe(1)
+    expect(wrapper.emitted()['page-change'][0]).toEqual([42])
   })
 })
