f95c4c03aae7943fdf262367e4bb80c2379e0105
François Hervieux <francois.hervieux@gmail.com>
2021-02-27 00:02:47 +0100

    Add a custom page component example

---
+++ docs/.vuepress/components/WithCustomPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <sliding-pagination
+    pageComponent="text-input-custom-page"
+    :current="current"
+    :total="total"
+    @page-change="onPageChange"
+  >
+  </sliding-pagination>
+</template>
+
+<script>
+import SlidingPagination from '@/../../../src/SlidingPagination.vue'
+
+export default {
+  name: 'WithCustomPage',
+  components: {
+    SlidingPagination
+  },
+  data() {
+    return {
+      current: 1,
+      total: 20
+    }
+  },
+  methods: {
+    onPageChange (page) {
+      this.current = page
+    }
+  }
+}
+</script>
